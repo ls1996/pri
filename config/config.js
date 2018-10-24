4a814435eaa9040ef9f9aae87936e9a22228ef34
ls1996 <31238320+ls1996@users.noreply.github.com>
2018-10-24 17:16:09 +0800

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,32 @@
+export default {
+    singular: true,
+    plugins: [
+        ['umi-plugin-react', {
+            antd:true,
+            dva:true,
+        }],
+    ],
+    routes: [{
+        path: '/',
+        component: '../layout',
+        routes:[
+            {
+                path:'/',
+                component:'HelloWorld'
+            },
+            {
+                path:"/happy",
+                component:"Happy"
+            },
+            {
+                path:"/dashboard",
+                routes: [
+                    {path: "/dashboard/analysis", component: "Dashboard/Analysis"},
+                    {path: "/dashboard/monitor", component: "Dashboard/Monitor"},
+                    {path: "/dashboard/workplace", component: "Dashboard/Workplace"}
+                ]
+            },
+            
+        ]
+    }]
+};
